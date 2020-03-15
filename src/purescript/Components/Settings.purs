373af68a3f6b0b1567b68f4c73e83d4a24a82848
Giulio Cesare Solaroli <giulio.cesare@solaroli.it>
2020-03-15 16:05:29 +0100

    Interim chages (that compile)

--- src/purescript/Components/Settings.purs
+++ src/purescript/Components/Settings.purs
@@ -7,6 +7,7 @@ import Control.Applicative (pure, (<$>))
 import Control.Bind (bind, discard)
 import Control.Category (identity)
 import Control.Monad (class Monad)
+import Control.Monad.State.Class (class MonadState)
 import Control.Semigroupoid ((<<<), (>>>))
 import DOM.HTML.Indexed (HTMLbutton, HTMLinput)
 import DOM.HTML.Indexed.InputType (InputType(..))
@@ -14,6 +15,8 @@ import Data.Boolean (otherwise)
 import Data.Const (Const)
 import Data.Either (Either(..), either)
 import Data.Function (($), (#), const)
+import Data.Functor (void)
+import Data.Functor.Variant (VariantF)
 import Data.Int as Int
 import Data.Maybe (Maybe(..), maybe)
 import Data.Newtype (class Newtype)
@@ -22,6 +25,7 @@ import Data.Semigroup ((<>))
 import Data.Show (show)
 import Data.Symbol (SProxy(..))
 import Data.Unit (Unit, unit)
+import Data.Variant.Internal (FProxy)
 import Data.Void (Void)
 import Effect.Aff (Aff, Milliseconds(..), delay)
 import Effect.Aff.Class (class MonadAff, liftAff)
@@ -33,31 +37,28 @@ import Formless.Query (injQuery)
 import Formless.Validation (Validation(..), hoistFnE_)
 import Halogen as Formless.Types.Component
 import Halogen as Halogen
+import Halogen.Component (ComponentSlot)
 import Halogen.HTML as HTML
 import Halogen.HTML.Events as HTML.Events
 import Halogen.HTML.Properties as HTML.Properties
-import Main (defaultInput)
 
 type    Surface     = HTML.HTML
 data    Action      = NoAction
---                  | UpdateLength
                     | HandleForm State
                     | Click
---                  | HandleSettingsForm Settings
 data    Query a     = GetSettings (Settings -> a)
 type    Input       = Settings
 data    Output      = UpdatedSettings Settings
                     | RegeneratePassword
---type    Output     = Settings
 type    State       = Settings
 type    Slot        = Halogen.Slot Query Output
 
--- type Slots :: (Type -> Type) -> Type -> Type -> Type
-type    Slots = (
-    --          Formless.Slot :: Slot (Query form query slots) msg
-    -- formless :: Formless.Slot (Query form query slots) msg
---  formless :: Formless.Slot' 
-)
+type    ChildSlots  = ()
+type    MessyType_1 = (Const Void)
+type    MessyType_2 = Unit
+
+-- type Slots = ( formless ∷ Slot (VariantF ( query :: FProxy (Formless.QueryF Form ()) , userQuery :: FProxy (Const Void) ) ) State Unit )
+-- type Slots = ( formless ∷ Slot (VariantF ( query :: FProxy (Formless.QueryF Form ()) , userQuery :: FProxy (Const Void) ) ) State Unit )
 
 data    CharacterSet = CapitalLetters | LowercaseLetters | Digits | Space | Symbols
 type    Settings = {
@@ -91,36 +92,35 @@ formInput = {
 }
 
 --            :: Formless.Component    Component HTML (Query form query slots) input msg m
-formComponent :: forall m. MonadAff m => Formless.Component Form (Const Void) () Unit State m
-formComponent = Formless.component (const defaultInput) $ Formless.defaultSpec {
-        render = renderForm,
-        handleEvent = Formless.raiseResult
+formComponent :: forall m. MonadAff m => Formless.Component Form MessyType_1 ChildSlots MessyType_2 State m
+formComponent = Formless.component (const formInput) $ Formless.defaultSpec {
+        handleEvent = Formless.raiseResult,
+        render = renderForm
     }
     where
-    renderForm { form } = UIChunks.formContent_ [
-        UIChunks.input {
-            label: "Password Length",
-            help: Formless.getResult _length form # UIChunks.resultToHelp "How long do you want your password to be?",
-            -- help: Formless.getResult prx.length form # UIChunks.resultToHelp "How long do you want your password to be?",
-            placeholder: "32"
-        } [
-            -- HTML.Properties.value $ Formless.getInput prx.length form,
-            HTML.Properties.value $ Formless.getInput _length form,
-            --HTML.Events.onValueInput $ Just <<< Formless.asyncSetValidate (Milliseconds 500.0) prx.length
-            HTML.Events.onValueInput $ Just <<< Formless.asyncSetValidate (Milliseconds 500.0) _length
-        ],
-        UIChunks.buttonPrimary
-            [ HTML.Events.onClick \_ -> Just Formless.submit ]
-            [ HTML.text "Submit" ]
-    ]
-        where
-        --prx = Formless.mkSProxies (Formless.FormProxy :: _ Form)
-        _length = SProxy :: SProxy "length"
+        renderForm { form } = UIChunks.formContent_ [
+            UIChunks.input {
+                label: "Password Length",
+                help: Formless.getResult _length form # UIChunks.resultToHelp "How long do you want your password to be?",
+                placeholder: "32"
+            } [
+                HTML.Properties.value $ Formless.getInput _length form,
+                HTML.Events.onValueInput $ Just <<< Formless.asyncSetValidate (Milliseconds 500.0) _length
+            ],
+            UIChunks.buttonPrimary
+                [ HTML.Events.onClick \_ -> Just Formless.submit ]
+                [ HTML.text "Submit" ]
+        ]
+            where
+            _length = SProxy :: SProxy "length"
 
 -- ###########################################################################################
 
 initialState :: Input -> State
 initialState = identity
+
+-- mkComponent :: ∀ surface state query action slots input output m. ComponentSpec surface state query action slots input output m → Component surface query input output m
+-- mkComponent :: ∀ surface state query action slots input output m. { eval ∷ forall a. HalogenQ query action input a -> HalogenM state action slots output m a , initialState ∷ input -> state , render ∷ state -> surface (ComponentSlot surface slots m action) action } → Component surface query input output m
 
 component :: forall m. MonadAff m => Halogen.Component Surface Query Input Output m
 component = Halogen.mkComponent {
@@ -133,43 +133,31 @@ component = Halogen.mkComponent {
         initialize   = initialize,      --  initialize      :: Maybe Action
         finalize     = finalize         --  finalize        :: Maybe Action
     }
-                    -- :: HalogenQ Query Action Input ~> HalogenM State Action Slots Output m
 }
 
-render :: forall m. MonadAff m => State -> Halogen.ComponentHTML Action Slots m
+-- render :: ∀ m. MonadAff m ⇒ State → Surface (ComponentSlot Surface Slots m Action ) Action
 render ({length:length}) = HTML.div [HTML.Properties.class_ (Halogen.ClassName "settings")] [
     HTML.h1  [] [HTML.text (show length)],
---  HTML.input [HTML.Properties.name "length", HTML.Events.onChange updateLength, HTML.Events.onKeyDown updateLength, Halogen.HTML.Events.onKeyUp updateLength],
-    -- HTML.slot Formless._formless unit formComponent unit (Just <<< HandleForm),
+    HTML.slot Formless._formless unit formComponent unit (Just <<< HandleForm),
     HTML.button [HTML.Properties.title "new", HTML.Events.onClick \_ -> Just Click] [HTML.text "new"]
 ]
 
 -- updateLength :: forall a. a -> Maybe Action
 -- updateLength x = Just UpdateLength
 
-handleAction :: forall m. MonadAff m => Action → Halogen.HalogenM State Action Slots Output m Unit
+-- handleAction :: forall m. MonadAff m => Action -> Halogen.HalogenM State Action Slots Output m Unit
+handleAction :: forall m t35. MonadAff m => Action -> Halogen.HalogenM State Action t35 Output m Unit
 handleAction = case _ of
-    NoAction ->
-        pure unit
---  UpdateLength -> do
---      Halogen.raise RegeneratePassword
+    NoAction -> do
+      pure unit
     Click -> do
         Halogen.liftEffect $ log "Settings: click \"new\""
         Halogen.raise RegeneratePassword
---  HandleSettingsForm settings -> do
---      Halogen.liftEffect $ log "SettingsFork: " <> logShow (settings :: Settings)
---      pure unit
     HandleForm s -> do
         Halogen.liftEffect $ log "HandleForm: " <> logShow (s :: State)
-        -- pure unit
 
---  SubComponentOutput (SubComponent.S_NoOutput) ->
---      pure unit
---  SubComponentOutput (SubComponent.S_Click_Happened) ->
---      Halogen.modify_ (\(State counter) -> State (counter + 1))
 
-handleQuery :: forall m a. Query a -> Halogen.HalogenM State Action Slots Output m (Maybe a)
---handleQuery = const (pure Nothing)
+handleQuery :: forall m a. MonadState State m => Query a -> m (Maybe a)
 handleQuery = case _ of
     GetSettings k -> do
         --result::Settings <- Halogen.get
